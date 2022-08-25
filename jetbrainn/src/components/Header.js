eb7cacce6025d71c10eb90a1a77b5e18d19a1585
EmmanuelVictor62 <104584134+EmmanuelVictor62@users.noreply.github.com>
2022-08-25 17:01:43 +0100

    Removed Font Family

--- jetbrainn/src/components/Header.js
+++ jetbrainn/src/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Logo from '../Thumbnails/Jetbrain-logo.png'
 import '../styles.css/Header.css'
-import '../styles.css/media-query.css'
 
 const Header = () => {
   return (
